3b6eb2bccff193e091edba79170699d085c49a38
BleuMei <sandstormwindchimes@gmail.com>
2026-04-14 04:28:46 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,12 +2,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/fireba
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_DOMAIN",
-  projectId: "YOUR_ID",
-  storageBucket: "YOUR_BUCKET",
-  messagingSenderId: "YOUR_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBMc36-Vg_Ger814ZWz_JHs7KG-csgGggA",
+  authDomain: "vermeil-quest.firebaseapp.com",
+  projectId: "vermeil-quest",
+  storageBucket: "vermeil-quest.firebasestorage.app",
+  messagingSenderId: "943194791633",
+  appId: "1:943194791633:web:a3ddd0f3914f518171aff0",
+  measurementId: "G-02CJBN8HS9"
 };
 
 const app = initializeApp(firebaseConfig);
